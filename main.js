8d088281b3d07bfcd4c55c6ade2e97f614e477f6
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-18 08:54:18 -0500

    2

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 addEventListener(`DOMContentLoaded`,()=>{
     // # DEFINIR VARIABLES NUEMRIAS
     // # DATOS DE ENTRADA
-    // ## OPERACIONES BASICAS: + *
-    // from math import sqrt
-    let x1=Number(prompt(`ingrese el valor de x1 `))
-    let x2=Number(prompt(`ingrese el valor de x2 `)) 
-    let y1=Number(prompt(`ingrese el valor de y1 `))
-    let y2=Number(prompt(`ingrese el valor de y2 `))
-    // #PROCESOS U OPERACIONES
+    // ## OPERACIONES BASICAS: + 
+    let pr1=Number(prompt(`dijite el valor del producto# 1:`))
+    let pr2=Number(prompt(`dijite el valor del producto# 2:`))
+    let pr3=Number(prompt(`dijite el valor del producto# 3:`))
+    let pr4=Number(prompt(`dijite el valor del producto# 4:`))
+    let pr5=Number(prompt(`dijite el valor del producto# 5:`))
+     // #PROCESOS U OPERACIONES
 
-    let res=Number((x2-x1)**2+(y2-y1)**2)
-
-    // #salida o entrada de resultado
-    console.log(`la distancia de los puntos es ${res}`);
+    let des1=pr1-(pr1*0.05)
+    let des2=pr2-(pr2*0.05)
+    let des4=pr4-(pr4*0.02)
+    let des5=pr5-(pr5*0.02)
+    let des6= des1+des2+pr3+des4+des5
+    // #s|alida o entrada de resultado
+    console.log( `l valor de los 2 primeros productos son: ${des1}  y${des2} `);
+    console.log( `el valor del tercer producto es: ${pr3}`);
+    console.log( `el valor de los 2 ultimos productos sos:,${des4} , y, ${des5}`);
+    console.log( `el valor de toda la compra es de: ${des6}`);
 })
