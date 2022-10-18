8f77cbd8080ccd0363eadffa0f5077cab1808357
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-18 08:55:10 -0500

    4

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 addEventListener(`DOMContentLoaded`,()=>{
     // # DEFINIR VARIABLES NUEMRIAS
-    // # DATOS DE ENTRADA
-    // ## OPERACIONES BASICAS: + /
-    let madre=Number(prompt(`edad de la Mamá:`))
-    let hijo=Number(prompt(`edad del hijo:`))
-    // #PROCESOS U OPERACIONES
-    let res=(madre-hijo)
-    // #salida o entrada de resultado
-    console.log(`edad de la madre en el parto: ${res}`);
+// # DATOS DE ENTRADA
+// ## OPERACIONES BASICAS: + /
+let llavesh=Number(11500) 
+let bomba=Number(1168000) 
+let pernos=Number(87000) 
+let cambio=Number(91000) 
+// #PROCESOS U OPERACIONES
+let res1=(llavesh*5)
+let res2=(pernos*3)
+let res3=(res1+res2+bomba+cambio)
+// #salida o entrada de resultado
+console.log(`total de dinaro que tenia:${res3}`);
 })
