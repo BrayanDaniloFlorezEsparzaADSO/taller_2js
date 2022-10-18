27ca37043c2052e8f7eb888c90c725194fbb81f5
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-18 08:55:33 -0500

    5

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 addEventListener(`DOMContentLoaded`,()=>{
     // # DEFINIR VARIABLES NUEMRIAS
-// # DATOS DE ENTRADA
-// ## OPERACIONES BASICAS: + /
-let llavesh=Number(11500) 
-let bomba=Number(1168000) 
-let pernos=Number(87000) 
-let cambio=Number(91000) 
-// #PROCESOS U OPERACIONES
-let res1=(llavesh*5)
-let res2=(pernos*3)
-let res3=(res1+res2+bomba+cambio)
-// #salida o entrada de resultado
-console.log(`total de dinaro que tenia:${res3}`);
+    // # DATOS DE ENTRADA
+    // ## OPERACIONES BASICAS: + /
+    let grch=Number(prompt(`ingrese el numero de gr de el chocolate`))
+    let grharina=100
+    let grcacao=10
+    let pnuez=1
+    // #PROCESOS U OPERACIONES
+    let res=grharina-grch
+    // #salida o entrada de resultado
+    console.log(`la cantidad de gramos de harina es de :${res}`);
 })
