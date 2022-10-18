d5f390ab9ba80ba0036e502ab0bca6b08c2d90ee
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-18 08:56:00 -0500

    6

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 addEventListener(`DOMContentLoaded`,()=>{
-    // # DEFINIR VARIABLES NUEMRIAS
-    // # DATOS DE ENTRADA
-    // ## OPERACIONES BASICAS: + /
-    let grch=Number(prompt(`ingrese el numero de gr de el chocolate`))
-    let grharina=100
-    let grcacao=10
-    let pnuez=1
+    // # DEFINIR VARIABLES| NUEMRIAS
+    // # DATOS DE ENTRADA|
+    // ## OPERACIONES BASICAS:| + /
+    let cajas=Number(25)
+    let kilos=Number(748)
     // #PROCESOS U OPERACIONES
-    let res=grharina-grch
+    let res=(cajas*kilos)
     // #salida o entrada de resultado
-    console.log(`la cantidad de gramos de harina es de :${res}`);
+    console.log(`numero de kilos que transporta: ${res} kg`);
 })
