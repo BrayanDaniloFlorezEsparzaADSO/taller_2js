a942878add7b9e810aced2ec673a1f753b937a58
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-18 08:57:40 -0500

    9

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 addEventListener(`DOMContentLoaded`,()=>{
     // # DEFINIR VARIABLES NUEMRIAS
     // # DATOS DE ENTRADA
-    // ## OPERACIONES BASICAS: + /
-    let precio=Number(prompt(`ingrese el valor por m2`))
-    let metros=Number(prompt(`ingrese los m2 que quiere pintar`))
+    // ## OPERACIONES BASICAS: + 
+    let nacimiento=Number(prompt(`dijite su año de nacimiento:`))
+    let fechaactual=Number(prompt(`dijite el año actual:`))
     // #PROCESOS U OPERACIONES
-    let res=metros*precio
+    let res=(fechaactual-nacimiento)
     // #salida o entrada de resultado
-    console.log(`el valor del cuadro es de: ${res}`);
+    console.log(`su edad es: ${res}`);
 })
